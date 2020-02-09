b271e9c79572fdd20178388938d35af56a8d7d2c
suoto <andre820@gmail.com>
2020-02-09 13:29:36 +0000

    Data width = 1 takes too long, run them on different sims

--- testbench/axi_bit_interleaver_tb.vhd
+++ testbench/axi_bit_interleaver_tb.vhd
@@ -18,8 +18,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with DVB FPGA.  If not, see <http://www.gnu.org/licenses/>.
 
--- vunit: run_all_in_same_sim
-
 use std.textio.all;
 
 library ieee;
