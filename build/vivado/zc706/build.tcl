357fbd9b7773eaf1ee17ed4c4a4a2afabd77a0bd
suoto <andre820@gmail.com>
2021-08-14 18:00:09 +0100

    Updating build scripts

--- build/vivado/zc706/build.tcl
+++ build/vivado/zc706/build.tcl
@@ -59,4 +59,5 @@ wait_on_run synth_1
 
 launch_runs impl_1 -to_step write_bitstream -jobs 8
 wait_on_run impl_1
-write_hwdef -force zc706/dvbs2_encoder.hdf
+
+write_hw_platform -fixed -force zc706/dvbs2_encoder.xsa
